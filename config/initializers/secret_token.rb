16dc4758c6585ecf7901d582f78f5649b4e77047
Juan Pablo Balarini <jpbalarini@gmail.com>
2013-09-04 12:10:29 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neonplay::Application.config.secret_token = '7d1fb8ea0da9ff31c42d0458cb90a02ddbe1c6608b1e1a4cd72a8c3fcd6faba0af2e3576c71773c50a7a718d36de5d292746529c94a1e1ca396a6b4e2c236879'
